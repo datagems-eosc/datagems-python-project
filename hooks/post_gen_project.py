aa0fae4d87d449ab8525bc65c7c1bbbbf58e74d5
MikeXydas <mikexydas@gmail.com>
2025-03-20 16:28:34 +0200

    remove codegen

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -24,26 +24,12 @@ def move_dir(src: str, target: str) -> None:
 
 
 if __name__ == "__main__":
-    if "{{cookiecutter.include_github_actions}}" != "y":
-        remove_dir(".github")
-    else:
-        if "{{cookiecutter.mkdocs}}" != "y" and "{{cookiecutter.publish_to_pypi}}" == "n":
-            remove_file(".github/workflows/on-release-main.yml")
-
     if "{{cookiecutter.mkdocs}}" != "y":
         remove_dir("docs")
         remove_file("mkdocs.yml")
 
     if "{{cookiecutter.dockerfile}}" != "y":
         remove_file("Dockerfile")
-
-    if "{{cookiecutter.codecov}}" != "y":
-        remove_file("codecov.yaml")
-        if "{{cookiecutter.include_github_actions}}" == "y":
-            remove_file(".github/workflows/validate-codecov-config.yml")
-
-    if "{{cookiecutter.devcontainer}}" != "y":
-        remove_dir(".devcontainer")
 
     if "{{cookiecutter.open_source_license}}" == "MIT license":
         move_file("LICENSE_MIT", "LICENSE")
